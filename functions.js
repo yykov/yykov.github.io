837002c5876a0f69af1647aafab836d019e05067
Leiver Bravo M <lnbm.1316670338@gmail.com>
2021-12-15 17:06:59 -0500

    Avance 2

--- functions.js
+++ functions.js
@@ -1,29 +1,65 @@
-/**Declaración de funciones */
-function enviar(){
-    alert("!Enviado Correctamente!");
+// Funcionalidad del modo claro/oscuro
+const btnSwitch = document.querySelector('#switch');
+
+btnSwitch.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+    btnSwitch.classList.toggle('active');
+    btnShopCart.classList.toggle('active');
+    btnProfile.classList.toggle('active');
+})
+
+const btnShopCart = document.querySelector('#shop-cart');
+
+btnShopCart.addEventListener('click', () => {
+    document.btnShopCart.classList.toggle('cart-active');
+})
+
+const btnProfile = document.querySelector('#profile');
+
+btnProfile.addEventListener('click', () => {
+    btnProfile.classList.toggle('pro-active');
+})
+
+// Funcionalidad del Slider
+const slider = document.querySelector('#slider');
+let sliderSection = document.querySelectorAll('.slider__section');
+let sliderSectionLast = sliderSection[sliderSection.length - 1];
+
+const btnLeft = document.querySelector('#btn-left');
+const btnRight = document.querySelector('#btn-right');
+
+slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+
+function Next() {
+    let sliderSectionFirst = document.querySelectorAll('.slider__section')[0];
+    slider.style.marginLeft = '-200%';
+    slider.style.transition = 'all 0.5s';
+    setTimeout(function(){
+        slider.style.transition = 'none';
+        slider.insertAdjacentElement('beforeend', sliderSectionFirst);
+        slider.style.marginLeft = '-100%';
+    }, 500);
 }
 
-function checkEmail() {
-    var email = document.getElementById('mail');
-    var filter = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (!filter.test(email.value)) {
-        alert('Por favor ingresa un correo electrónico valido!');
-        email.focus;
-        return false;
-    }
+btnRight.addEventListener('click', function(){
+    Next();
+});
+
+function Preview() {
+    let sliderSectionLast = document.querySelectorAll('.slider__section')[3];
+    slider.style.marginLeft = '0';
+    slider.style.transition = 'all 0.5s';
+    setTimeout(function(){
+        slider.style.transition = 'none';
+        slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+        slider.style.marginLeft = '-100%';
+    }, 500);
 }
 
-function control(){
-    if(document.getElementById('name') == null || document.getElementById('uname')==""){
-        alert("El campo no puede estar vacío.");
-        document.getElementById('name').focus();
-        return false;
-    }
-    return true;
-}
-function perifericos(){
-    alert("No disponible por el momento :(");
-}
-function celulares(){
-    alert("No disponible por el momento :(");
-}
+btnLeft.addEventListener('click', function(){
+    Preview();
+});
+
+setInterval(function(){
+    Next();
+}, 9000);
